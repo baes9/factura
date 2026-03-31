a6bfa3b54cf723553b60f00525bc33e097ef478d
baes9 <braddleyestrella@gmail.com>
2026-03-31 15:39:33 -0500

    Commit 4

--- src/main/java/com/sistema/factura/Controllers/CiudadController.java
+++ src/main/java/com/sistema/factura/Controllers/CiudadController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/api/ciudad")
 @RequiredArgsConstructor
 public class CiudadController {
+    @Autowired
     private CiudadService ciudadService;
     
     @PostMapping
--- src/main/java/com/sistema/factura/Services/CiudadService.java
+++ src/main/java/com/sistema/factura/Services/CiudadService.java
@@ -3,6 +3,7 @@ package com.sistema.factura.Services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import com.sistema.factura.Entities.Ciudad;
 @Service
 @NoArgsConstructor
 public class CiudadService {
+    @Autowired
     private CiudadRepository ciudadRepository;
 
     public Ciudad guardarCiudad(Ciudad ciudad) {
