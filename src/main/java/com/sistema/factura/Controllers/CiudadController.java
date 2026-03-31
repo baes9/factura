587ad3d5fab7e9cb75bc550822827e98da8be16f
baes9 <braddleyestrella@gmail.com>
2026-03-31 14:47:53 -0500

    Prueba 2 - Commit 2

--- src/main/java/com/sistema/factura/Controllers/CiudadController.java
+++ src/main/java/com/sistema/factura/Controllers/CiudadController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
 @RestController
 @RequestMapping("/api/ciudad")
 @RequiredArgsConstructor
